ecba37e94cac77622eaa8f6e90eeb7acb28999c4
Yakup Geyik <yakuppgeyik@gmail.com>
2022-06-06 23:45:37 +0300

    Get all tasks and delete task by id added

--- app/routers/task.router.js
+++ app/routers/task.router.js
@@ -5,6 +5,16 @@ const Task = require('../models/task.model')
 router.post('/task', async (req, res) => {
   const task = new Task({ ...req.body, userId: 'abc123' })
 
+  const tasksHaveHighestOrder = await Task.find({}).sort({ order: -1 }).limit(1)
+
+  if (tasksHaveHighestOrder.length > 0) {
+    const highestOrder = tasksHaveHighestOrder[0].order
+
+    task.order = highestOrder + 1
+    console.log(highestOrder)
+    console.log(task)
+  }
+
   try {
     await task.save()
     res.status(201).send(task)
@@ -19,6 +29,20 @@ router.get('/task', async (req, res) => {
     res.send(tasks)
   } catch (error) {
     res.send(error)
+  }
+})
+
+router.delete('/task', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.query.id)
+
+    if (!task) {
+      return res.status(404).send()
+    }
+
+    res.send(task)
+  } catch (error) {
+    res.status(500).send(error)
   }
 })
 
